3f0270b4d497bfc80a8e8c188b6b07f1b99d887e
Enmanuel Reyes <enmanuel_reyes@claro.com.do>
2017-05-04 15:30:37 -0400

    Reto 6

---
+++ Reto6/Emotions/ServiceEmotions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.ProjectOxford.Emotion;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emotions
+{
+    public class ServiceEmotions
+    {
+        static string key = "15a4e1fa2b0444aebecd9676627fd3c0";
+        public static async Task<Dictionary<string, float>> GetEmotions(Stream stream)
+        {
+            EmotionServiceClient cliente = new EmotionServiceClient(key);
+            var emotions = await cliente.RecognizeAsync(stream);
+
+            if (emotions == null || emotions.Count() == 0)
+                return null;
+
+            return emotions[0].Scores.ToRankedList().ToDictionary(x => x.Key, x => x.Value);
+        }
+    }
+}
---
+++ Reto6/Reto6/CognitiveActivity.cs
@@ -0,0 +1,126 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Emotions;
+using System.IO;
+using Plugin.Media.Abstractions;
+using static Emotions.ItemManager;
+
+namespace Reto6
+{
+    [Activity(Label = "Registrar datos")]
+    public class CognitiveActivity : Activity
+    {
+        ItemManager manager;
+        static Stream streamCopy;
+        string ResultadoEmociones = "Reto6 + DO + Enmanuel: ";
+        TextView txtResultado;
+        Button btnRegistraResultados;
+        Button btnAnalizaFoto;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Microsoft.WindowsAzure.MobileServices.CurrentPlatform.Init();
+            manager = ItemManager.DefaultManager;
+            SetContentView(Resource.Layout.Cognitive);
+
+            Button btnCamara = FindViewById<Button>(Resource.Id.btnCamara);
+            btnAnalizaFoto = FindViewById<Button>(Resource.Id.btnAnalizaFoto);
+            btnRegistraResultados = FindViewById<Button>(Resource.Id.btnRegistraResultados);
+            txtResultado = FindViewById<TextView>(Resource.Id.txtOutput);
+
+            btnRegistraResultados.Visibility = ViewStates.Invisible;
+            btnAnalizaFoto.Visibility = ViewStates.Invisible;
+            btnCamara.Click += btnCamara_Click;
+            btnAnalizaFoto.Click += btnAnalizaFoto_Click;
+            btnRegistraResultados.Click += btnRegistraResultados_Click;
+        }
+
+        private async void btnRegistraResultados_Click(object sender, EventArgs e)
+        {
+            btnRegistraResultados.Visibility = ViewStates.Invisible;
+            Toast.MakeText(this, "Registrando tus resultados", ToastLength.Short).Show();
+            TorneoItem registro = new TorneoItem();
+            registro.DeviceId = Android.Provider.Settings.Secure.GetString(ContentResolver, Android.Provider.Settings.Secure.AndroidId);
+            registro.Email = "enmanuelreyes@live.com";
+            registro.Reto = ResultadoEmociones;
+            Toast.MakeText(this, "Enviando tu registro", ToastLength.Short).Show();
+            await manager.SaveTaskAsync(registro);
+            Toast.MakeText(this, "Gracias por registrarte", ToastLength.Long).Show();
+
+        }
+        private async void btnAnalizaFoto_Click(object sender, EventArgs e)
+        {
+            if (streamCopy != null)
+            {
+                btnAnalizaFoto.Visibility = ViewStates.Invisible;
+                Toast.MakeText(this, "Analizando imagen utilizando Cognitive Services", ToastLength.Short).Show();
+                Dictionary<string, float> emotions = null;
+                try
+                {
+                    streamCopy.Seek(0, SeekOrigin.Begin);
+                    emotions = await ServiceEmotions.GetEmotions(streamCopy);
+                }
+                catch (Exception ex)
+                {
+                    Toast.MakeText(this, "Se ha presentado un error al conectar con los servicios", ToastLength.Short).Show();
+                    return;
+                }
+
+                StringBuilder sb = new StringBuilder();
+                if (emotions != null)
+                {
+                    txtResultado.Text = "---Analisis de Emociones---";
+                    sb.AppendLine();
+                    foreach (var item in emotions)
+                    {
+                        string toAdd = item.Key + " : " + item.Value + " ";
+                        sb.Append(toAdd);
+                    }
+                    txtResultado.Text += sb.ToString();
+                    btnRegistraResultados.Visibility = ViewStates.Visible;
+                }
+                else txtResultado.Text = "---No se detectó una cara---";
+                ResultadoEmociones += sb.ToString();
+            }
+            else txtResultado.Text = "---No has seleccionado una imagen---";
+        }
+        private async void btnCamara_Click(object sender, EventArgs e)
+        {
+            MediaFile file = null;
+            try
+            {
+                file = await ServiceImage.TakePicture(true);
+            }
+            catch (Android.OS.OperationCanceledException)
+            {
+
+            }
+            SetImageToControl(file);
+            btnAnalizaFoto.Visibility = ViewStates.Visible;
+        }
+
+        private void SetImageToControl(MediaFile file)
+        {
+            if (file == null)
+            {
+                return;
+            }
+            ImageView imgImage = FindViewById<ImageView>(Resource.Id.imageViewFoto);
+            imgImage.SetImageURI(Android.Net.Uri.Parse(file.Path));
+            var stream = file.GetStream();
+            streamCopy = new MemoryStream();
+            stream.CopyTo(streamCopy);
+            stream.Seek(0, SeekOrigin.Begin);
+            file.Dispose();
+        }
+    }
+}
